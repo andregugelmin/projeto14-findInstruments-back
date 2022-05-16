49f7522f3c47361bfe91605d7cc4e4b579a5aac3
andregugelmin <andregugelmin1996@gmail.com>
2022-05-16 12:45:53 -0300

    fix: removed token validation from products router

--- Routers/productsRouter.js
+++ Routers/productsRouter.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import { getProducts } from './../Controllers/productsController.js';
-import { validToken } from './../Middlewares/userTokenValidation.js';
 
 const productRouter = Router();
 
-productRouter.get('/products', validToken, getProducts);
+productRouter.get('/products', getProducts);
 
 export default productRouter;
